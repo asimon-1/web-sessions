169ffc2ef4749981709ed9b662c3c03b1dc9faa9
asimon-1 <40246417+asimon-1@users.noreply.github.com>
2022-01-07 17:21:32 -0500

    Move session to a thread

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,90 @@
 #![feature(proc_macro_hygiene)]
+#![feature(const_panic)]
+#![feature(const_mut_refs)]
 
 use skyline::install_hook;
-use skyline_web::{Webpage, WebSession, Visibility};
-use smash::lib::lua_const::*;
+use skyline_web::{Visibility, Webpage};
 use smash::app::{self, lua_bind::*};
-use nnsdk::web::offlinewebsession::OfflineWebSession;
+use smash::lib::lua_const::*;
 
-static SESSION: WebSession = start_session();
+static mut SHOW_SESSION: bool = false;
 
-fn start_session() -> WebSession {
-    let render = r#"<!doctype html>
-    <html>
-        <head>
-            <title>Web Session Test</title>
-        </head>
-        <body>
-            <p>This is a test of the skyline-web session functionality</p>
-            <div>
-                <p>Send message: </p> <input type="text" id="send_content"
-                    name="send_content">
-                <button type="button" , onclick="send_message();">SEND</button>
-            </div>
-            <div>
-                <p>Receive message: </p><div id="receive_content"></div>
-            </div>
-        </body>
-        <script>
-            function myFunction() {
-                var content = document.getElementById("send_content").textContent;
-                window.nx.send(content);
+fn log(msg: &str) {
+    let plugin_name = "web-session";
+    println!("[{}] {}", plugin_name, msg);
+}
+
+fn start_session() {
+    std::thread::spawn(|| {
+        let render = r#"<!doctype html>
+        <html>
+            <head>
+                <title>Web Session Test</title>
+            </head>
+            <body>
+                <p>This is a test of the skyline-web session functionality</p>
+                <div>
+                    <p>Send message: </p> <input type="text" id="send_content"
+                        name="send_content">
+                    <button type="button" , onclick="send_message();">SEND</button>
+                </div>
+                <div>
+                    <p>Receive message: </p><div id="receive_content"></div>
+                </div>
+            </body>
+            <script>
+                function send_message() {
+                    var content = document.getElementById("send_content").textContent;
+                    window.nx.send(content);
+                }
+                function goBack() {
+                    location.href = "http://localhost/";
+                    window.history.back();
+                }
+                function closeSession() {
+                    window.nx.endApplet();
+                }
+        
+                window.nx.addEventListener("message", function (e) {
+                document.getElementById("receive_content").innerHTML = e.data;
+                });
+                nx.footer.setAssign("B", "Go Back", goBack);
+            </script>
+        </html>"#;
+        let session = Webpage::new()
+            .htdocs_dir("contents")
+            .file("index.html", &render)
+            .open_session(Visibility::InitiallyHidden)
+            .unwrap();
+        loop {
+            std::thread::sleep(std::time::Duration::from_millis(100));
+            unsafe {
+                if SHOW_SESSION {
+                    SHOW_SESSION = false;
+                    session.send("Hello from Rust!");
+                    log("Sent message \"Hello from Rust!\"");
+
+                    log("Showing session");
+                    session.show();
+
+                    // session.send("test two");
+                    // log("Sent message \"test two\"");
+                    
+                    // log("Waiting for exit");
+                    // session.wait_for_exit();
+                    // log("Exited");
+                }
+                if let Some(msg) = session.try_recv_max(0x10000000) {
+                    log(&format!("Received message from JS: {}", msg));
+                }
             }
-    
-            window.nx.addEventListener("message", function (e) {
-            document.getElementById("receive_content").innerHTML = e.data;
-            }
-        </script>
-    </html>"#;
-    let session = Webpage::new()
-        .htdocs_dir("contents")
-        .file("index.html", &render)
-        .open_session(Visibility::InitiallyHidden).unwrap();
-    session
+        }
+    });
 }
 
 pub unsafe fn menu_condition(module_accessor: &mut smash::app::BattleObjectModuleAccessor) -> bool {
     ControlModule::check_button_on(module_accessor, *CONTROL_PAD_BUTTON_SPECIAL)
-        && ControlModule::check_button_on_trriger(
-            module_accessor,
-            *CONTROL_PAD_BUTTON_APPEAL_HI,
-        )
+        && ControlModule::check_button_on_trriger(module_accessor, *CONTROL_PAD_BUTTON_APPEAL_HI)
 }
 
 unsafe fn once_per_frame_per_fighter(
@@ -60,16 +96,9 @@ unsafe fn once_per_frame_per_fighter(
     }
 
     if menu_condition(module_accessor) {
-        loop {
-            if let Some(msg) = SESSION.try_recv() {
-                dbg!(msg);
-
-                SESSION.show();
-                SESSION.send("test");
-                SESSION.wait_for_exit();
-                break;
-            }
-        }
+        log("Menu condition triggered");
+        log(&format!("SHOW_SESSION: {}", SHOW_SESSION));
+        SHOW_SESSION = true;
     }
 }
 
@@ -86,6 +115,7 @@ pub unsafe fn handle_get_command_flag_cat(
 
 #[skyline::main(name = "web-sessions")]
 pub fn main() {
-    println!("[web-sessions] Initialized");
+    log("Initialized");
     install_hook!(handle_get_command_flag_cat);
+    start_session();
 }
